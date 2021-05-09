ef164bbc253db3b3c36c86fc448335ee4cbce917
KenFukui <79432343+KenFukui@users.noreply.github.com>
2021-05-09 14:24:20 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -39,8 +39,10 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    if prototype.destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
   end
